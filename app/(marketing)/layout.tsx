dde1cda64414d15dff21414dd5698e2be7bb6056
Francis Iwu <apkfem@gmail.com>
2024-02-07 12:29:40 +0100

    working on the app/market, components.json, components and lib folders

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,9 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-200">
+      <main className="pt-40 pb-20 bg-slate-200">{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
